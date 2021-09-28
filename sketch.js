29450fc91ad2a7525e570b256895684a4ad1f434
mikemarun <90887149+mikemarun@users.noreply.github.com>
2021-09-28 01:48:59 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,906 @@
+
+// when boxY_01>600 || boxY_01=0;
+let boxY_01= 175;
+let boxY_01Original = 175;
+let speed = 0;
+
+let box_01={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 50,
+  //posY: 175,
+  posY: -175,
+  posY_original: 175,
+  posX_original: 50,
+  width: 500,
+  height: 250,
+  speed: 6,
+};
+
+let boxY_02= 190;
+let boxY_02Original = 190;
+let speed2 = 0;
+
+let box_02={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 65,
+  //posY: 190,
+  posY: -790,
+  posY_original: 190,
+  posX_original: 65,
+  width: 470,
+  height: 220,
+  speed: 6,
+};
+
+let boxY_03= 230;
+let boxY_03Original = 230;
+let speed3 = 0;
+
+let box_03={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 105,
+  //posY: 230,
+  posY: -750,
+  posY_original: 230,
+  posX_original: 105,
+  width: 390,
+  height: 140,
+  speed: 6,
+};
+
+let circY_04= 230;
+let circY_04Original = 230;
+
+let circ_04={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 102,
+  //posY: 230,
+  posY: -750,
+  posY_original: 230,
+  posX_original: 102,
+  diameter: 60,
+  speed: 6,
+};
+
+let circY_05= 371;
+let circY_05Original = 371;
+
+let circ_05={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 102,
+  //posY: 371,
+  posY: -611,
+  posY_original: 371,
+  posX_original: 102,
+  diameter: 50,
+  speed: 6,
+};
+
+let circY_06= 230;
+let circY_06Original = 230;
+
+let circ_06={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 498,
+  //posY: 230,
+  posY: -750,
+  posY_original: 230,
+  posX_original: 498,
+  diameter: 60,
+  speed: 6,
+};
+
+let circY_07= 371;
+let circY_07Original = 371;
+
+let circ_07={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 498,
+  //posY: 371,
+  posY: -611,
+  posY_original: 371,
+  posX_original: 498,
+  diameter: 50,
+  speed: 6,
+};
+
+let eliY_BIG=291
+let eliY_BIGoriginal=291
+
+let eli_BIG={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 300,
+  //posY: 291,
+  posY: -1525,
+  posY_original: 291,
+  posX_original: 300,
+  width: 110,
+  height: 157,
+  speed: 6,
+};
+
+
+
+let eliY_08= 313;
+let eliY_08Original = 313;
+
+let eli_08={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 300,
+  //posY: 313,
+  posY: -1500,
+  posY_original: 313,
+  posX_original: 300,
+  width: 65,
+  height: 60,
+  speed: 6,
+};
+
+let eliY_09= 313;
+let eliY_09Original = 313;
+
+let eli_09={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 300,
+  //posY: 313,
+  posY: -1500,
+  posY_original: 313,
+  posX_original: 300,
+  width: 30,
+  height: 25,
+  speed: 6,
+};
+
+// let eliY_10= 313;
+// let eliY_10Original = 313
+//
+// let eli_10={
+//   colorR: 133,
+//   colorG: 187,
+//   colorB: 101,
+//
+//   posX: 300,
+//   posY: 313,
+//   posY_original: 313,
+//   posX_original: 300,
+//   width: 30,
+//   height: 25,
+//   speed: 0,
+// };
+
+let squaY_11= 287;
+let squaY_11Original = 287;
+
+let squa_11={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 265,
+  // posY: 287,
+  posY: -1525,
+  posY_original: 287,
+  posX_original: 265,
+  size: 29,
+  speed: 6,
+};
+
+let eliY_12= 270;
+let eliY_12Original = 270;
+
+let eli_12={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 300,
+  //posY: 270,
+  posY: -1543,
+  posY_original: 270,
+  posX_original: 300,
+  width: 65,
+  height: 60,
+  speed: 6,
+};
+
+let eliY_13= 270;
+let eliY_13Original = 270;
+
+let eli_13={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 300,
+  //posY: 270,
+  posY: -1543,
+  posY_original: 270,
+  posX_original: 300,
+  width: 30,
+  height: 25,
+  speed: 6,
+};
+
+let squaY_14= 270;
+let squaY_14Original = 270;
+
+let squa_14={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 314,
+  //posY: 270,
+  posY: -1543,
+  posY_original: 270,
+  posX_original: 314,
+  size: 18,
+  speed: 6,
+};
+
+let circY_15= 276;
+let circY_15Original = 276;
+
+let circ_15={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 310,
+  //posY: 276,
+  posY: -1537,
+  posY_original: 276,
+  posX_original: 310,
+  diameter: 20,
+  speed: 6,
+};
+
+let circY_16= 280.5;
+let circY_16Original = 280.5;
+
+let circ_16={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 324,
+  //posY: 280.5,
+  posY: -1532.5,
+  posY_original: 280.5,
+  posX_original: 324,
+  diameter: 20,
+  speed: 6,
+};
+
+let circY_17= 281;
+let circY_17Original = 281;
+
+let circ_17={
+  colorR: 17,
+  colorG: 140,
+  colorB: 79,
+
+  posX: 323,
+  //posY: 281,
+  posY: -1532,
+  posY_original: 281,
+  posX_original: 323,
+  diameter: 20,
+  speed: 6,
+};
+
+let boxY_18= 231;
+let boxY_18Original = 231;
+
+let box_18={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 290,
+  //posY: 231,
+  posY: -1582,
+  posY_original: 231,
+  posX_original: 290,
+  width: 7,
+  height: 120,
+  speed: 6,
+};
+
+let boxY_19= 231;
+let boxY_19Original = 231;
+
+let box_19={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 305,
+  //posY: 231,
+  posY: -1582,
+  posY_original: 231,
+  posX_original: 305,
+  width: 7,
+  height: 120,
+  speed: 6,
+};
+
+let boxY_20= 207;
+let boxY_20Original = 207;
+
+let box_20={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 97,
+  //posY: 207,
+  posY: -2006,
+  posY_original: 207,
+  posX_original: 97,
+  width: 12,
+  height: 42,
+  speed: 6,
+};
+
+let boxY_21= 351;
+let boxY_21Original = 351;
+
+let box_21={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 98,
+  //posY: 351,
+  posY: -1862,
+  posY_original: 351,
+  posX_original: 98,
+  width: 10,
+  height: 38,
+  speed: 6,
+};
+
+let boxY_22= 207;
+let boxY_22Original = 207;
+
+let box_22={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 493,
+  //posY: 207,
+  posY: -2006,
+  posY_original: 207,
+  posX_original: 493,
+  width: 12,
+  height: 42,
+  speed: 6,
+};
+
+let boxY_23= 351;
+let boxY_23Original = 351;
+
+let box_23={
+  colorR: 133,
+  colorG: 187,
+  colorB: 101,
+
+  posX: 493,
+  //posY: 351,
+  posY: -1862,
+  posY_original: 351,
+  posX_original: 493,
+  width: 10,
+  height: 38,
+  speed: 6,
+};
+
+let text_1={
+  posX: 110,
+  posY: -2000,
+  posY_original: 350,
+  speed: 6,
+};
+
+let text_2={
+  posX: 340,
+  posY: -2000,
+  posY_original: 250,
+  speed: 6,
+};
+
+function setup() {
+  // put setup code here
+  createCanvas(600, 600);
+  // speed = random(1,7);
+  //speed = 3;
+  //speed2 = 5;
+  //speed3 = 6;
+}
+
+function draw() {
+  // put drawing code here
+  background(220);
+  noStroke();
+
+//Action 01
+  moveBox(box_01);
+//Action 02
+  moveBox(box_02);
+  moveBox(box_03);
+  moveCirc(circ_04);
+  moveCirc(circ_05);
+  moveCirc(circ_06);
+  moveCirc(circ_07);
+//Action 03
+  moveEli(eli_BIG);
+//Action 04
+  moveEli(eli_08);
+  moveEli(eli_09);
+  moveSqua(squa_11);
+  moveEli(eli_12);
+  moveEli(eli_13);
+  moveSqua(squa_14);
+  moveCirc(circ_15);
+  moveCirc(circ_16);
+  moveCirc(circ_17);
+  moveBox(box_18);
+  moveBox(box_19);
+//Action 05
+  moveBox(box_20);
+  moveBox(box_21);
+  moveBox(box_22);
+  moveBox(box_23);
+//Action 06
+  moveText(text_1);
+  moveText(text_2);
+
+  // if (box_01.speed === 0) { // this is how I know the next shape should start moving
+  //   push();
+  //   translate(0, frameY);
+    //draw rect
+    //draw 4 circles
+    // moveCircle(circle01);
+    // moveCircle(circle02);
+    // moveCircle(circle03);
+    // moveCircle(circle04);
+    // pop();
+
+    // frameY = frameY + speed;
+  }
+
+
+function moveBox(geometry) {
+  fill(geometry.colorR, geometry.colorG, geometry.colorB)
+  if (geometry.posY > 600) {
+    geometry.posY = 0;
+  }
+
+  console.log(geometry.posY);
+
+  geometry.posY = geometry.posY + geometry.speed;
+  rect(geometry.posX, geometry.posY, geometry.width, geometry.height);
+
+  if (geometry.posY >= geometry.posY_original-2 && geometry.posY <= geometry.posY_original+2) {
+    geometry.speed = 0;
+  }
+  if (text_1.speed === 0) {
+    geometry.posX = geometry.posX + geometry.speed+6;
+  }
+
+}
+
+function moveCirc(geometry) {
+    fill(geometry.colorR, geometry.colorG, geometry.colorB)
+    if (geometry.posY > 600) {
+      geometry.posY = 0;
+    }
+
+    console.log(geometry.posY);
+
+    geometry.posY = geometry.posY + geometry.speed;
+    circle(geometry.posX, geometry.posY, geometry.diameter);
+
+    if (geometry.posY >= geometry.posY_original-2 && geometry.posY <= geometry.posY_original+2) {
+      geometry.speed = 0;
+    }
+    if (text_1.speed === 0) {
+      geometry.posX = geometry.posX + geometry.speed+6;
+    }
+
+}
+
+function moveEli(geometry) {
+  fill(geometry.colorR, geometry.colorG, geometry.colorB)
+  if (geometry.posY > 600) {
+    geometry.posY = 0;
+  }
+
+  console.log(geometry.posY);
+
+  geometry.posY = geometry.posY + geometry.speed;
+  ellipse(geometry.posX, geometry.posY, geometry.width, geometry.height);
+
+  if (geometry.posY >= geometry.posY_original-2 && geometry.posY <= geometry.posY_original+2) {
+    geometry.speed = 0;
+  }
+  if (text_1.speed === 0) {
+    geometry.posX = geometry.posX + geometry.speed+6;
+  }
+
+}
+
+function moveSqua(geometry) {
+  fill(geometry.colorR, geometry.colorG, geometry.colorB)
+  if (geometry.posY > 600) {
+    geometry.posY = 0;
+  }
+
+  console.log(geometry.posY);
+
+  geometry.posY = geometry.posY + geometry.speed;
+  square(geometry.posX, geometry.posY, geometry.size);
+
+  if (geometry.posY >= geometry.posY_original-2 && geometry.posY <= geometry.posY_original+2) {
+    geometry.speed = 0;
+  }
+  if (text_1.speed === 0) {
+    geometry.posX = geometry.posX + geometry.speed+6;
+  }
+
+}
+function moveText(geometry) {
+    fill(0);
+    if (geometry.posY > 600) {
+      geometry.posY = 0;
+    }
+
+    console.log(geometry.posY);
+
+    geometry.posY = geometry.posY + geometry.speed;
+    text(random([100],[200]),geometry.posX,geometry.posY)
+
+    if (geometry.posY >= geometry.posY_original-2 && geometry.posY <= geometry.posY_original+2) {
+      geometry.speed = 0;
+    }
+    if (text_1.speed === 0) {
+      geometry.posX = geometry.posX + geometry.speed+6;
+    }
+
+}
+
+//fill(color(133, 187, 101))
+//rect(50, boxY_01, 500, 250)
+//boxY_01 = boxY_01 + speed;
+
+// When boxY_01 > 600, then boxY has to go back to the top of the screen
+// When boxY_01 > 600, then boxY = 0;
+
+//   if (boxY_01 > 600) {
+//     boxY_01 = 0;
+//   }
+//   if (boxY_01 >= boxY_01Original-2 && boxY_01 <= boxY_01Original+2) {
+//     speed = 0;
+//   }
+//   console.log(boxY_01)
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   rect(65, boxY_02, 470, 220)
+//   boxY_02 = boxY_02+ speed2;
+//
+//   if (boxY_02 > 600) {
+//     boxY_02 = 0;
+//   }
+//   if (boxY_02 >= boxY_02Original-2 && boxY_02 <= boxY_02Original+2) {
+//     speed2 = 0;
+//   }
+//
+//
+//   fill(color(133, 187, 101))
+//   rect(105, boxY_03, 390, 140)
+//   boxY_03 = boxY_03+ speed3;
+//
+//   if (boxY_03 > 600) {
+//     boxY_03 = 0;
+//   }
+//   if (boxY_03 >= boxY_03Original-2 && boxY_03 <= boxY_03Original+2) {
+//     speed3 = 0;
+//   }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   circle( 102, circY_04, 60)
+//   circY_04 = circY_04 + speed;
+//
+//   if (circY_04 > 600) {
+//     circY_04 = 0;
+//   }
+//   if (circY_04 >= circY_04Original-2 && circY_04 <= circY_04Original+2) {
+//     speed = 0;
+//   }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   circle( 102, circY_05, 50)
+//   circY_05 = circY_05+ speed2;
+//
+//   if (circY_05 > 600) {
+//     circY_05 = 0;
+//   }
+//   if (circY_05 >= circY_05Original-2 && circY_05 <= circY_05Original+2) {
+//     speed2 = 0;
+//   }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   circle( 498, circY_06, 60)
+//   circY_06 = circY_06+ speed3;
+//
+//     if (circY_06 > 600) {
+//       circY_06 = 0;
+//     }
+//     if (circY_06 >= circY_06Original-2 && circY_06 <= circY_06Original+2) {
+//       speed3 = 0;
+//     }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   circle( 498, circY_07, 50)
+//   circY_07 = circY_07+ speed;
+//
+//       if (circY_07 > 600) {
+//         circY_07 = 0;
+//       }
+//       if (circY_07 >= circY_07Original-2 && circY_07 <= circY_07Original+2) {
+//         speed = 0;
+//       }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   ellipse(300, eliY_08, 110, 157)
+//   eliY_08 = eliY_08+ speed2;
+//
+//         if (eliY_08 > 600) {
+//           eliY_08 = 0;
+//         }
+//         if (eliY_08 >= eliY_08Original-2 && eliY_08 <= eliY_08Original+2) {
+//           speed2 = 0;
+//         }
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   ellipse(300, eliY_09, 65, 60)
+//   eliY_09 = eliY_09+ speed3;
+//
+//           if (eliY_09 > 600) {
+//             eliY_09 = 0;
+//           }
+//           if (eliY_09 >= eliY_09Original-2 && eliY_09 <= eliY_09Original+2) {
+//             speed3 = 0;
+//           }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   ellipse(300, eliY_10, 30, 25)
+//   eliY_10 = eliY_10+ speed;
+//
+//           if (eliY_10 > 600) {
+//             eliY_10 = 0;
+//           }
+//           if (eliY_10 >= eliY_10Original-2 && eliY_10 <= eliY_10Original+2) {
+//             speed = 0;
+//           }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   square(265, squaY_11, 29)
+//   squaY_11 = squaY_11+ speed2;
+//
+//           if (squaY_11 > 600) {
+//             squaY_11 = 0;
+//           }
+//           if (squaY_11 >= squaY_11Original-2 && squaY_11 <= squaY_11Original+2) {
+//             speed2 = 0;
+//           }
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   ellipse(300, eliY_12, 65, 60)
+//   eliY_12 = eliY_12+ speed3;
+//
+//           if (eliY_12 > 600) {
+//             eliY_12 = 0;
+//           }
+//           if (eliY_12 >= eliY_12Original-2 && eliY_12 <= eliY_12Original+2) {
+//             speed3 = 0;
+//           }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   ellipse(300, eliY_13, 30, 25)
+//   eliY_13 = eliY_13+ speed;
+//
+//             if (eliY_13 > 600) {
+//               eliY_13 = 0;
+//             }
+//             if (eliY_13 >= eliY_13Original-2 && eliY_13 <= eliY_13Original+2) {
+//               speed = 0;
+//             }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   square(314, squaY_14, 18)
+//   squaY_14 = squaY_14+ speed2;
+//
+//             if (squaY_14 > 600) {
+//               squaY_14 = 0;
+//             }
+//             if (squaY_14 >= squaY_14Original-2 && squaY_14 <= squaY_14Original+2) {
+//               speed2 = 0;
+//             }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   circle(310, circY_15, 20)
+//   circY_15 = circY_15+ speed3;
+//
+//         if (circY_15 > 600) {
+//           circY_15 = 0;
+//         }
+//         if (circY_15 >= circY_15Original-2 && circY_15 <= circY_15Original+2) {
+//           speed3 = 0;
+//         }
+//
+//
+//   fill(color(17, 140, 79))
+//   circle(324, circY_16, 20)
+//   circY_16 = circY_16+ speed;
+//
+//         if (circY_16 > 600) {
+//           circY_16 = 0;
+//         }
+//         if (circY_16 >= circY_16Original-2 && circY_16 <= circY_16Original+2) {
+//           speed = 0;
+//         }
+//
+//
+//
+//   fill(color(17, 140, 79))
+//   circle(323, circY_17, 20)
+//   circY_17 = circY_17+ speed2;
+//
+//         if (circY_17 > 600) {
+//           circY_17 = 0;
+//         }
+//         if (circY_17 >= circY_17Original-2 && circY_17 <= circY_17Original+2) {
+//           speed2 = 0;
+//         }
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   rect(290, boxY_18, 7, 120)
+//   boxY_18 = boxY_18+ speed3;
+//
+//   if (boxY_18 > 600) {
+//     boxY_18 = 0;
+//   }
+//   if (boxY_18 >= boxY_18Original-2 && boxY_18 <= boxY_18Original+2) {
+//     speed3 = 0;
+//   }
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   rect(305, boxY_19, 7, 120)
+//   boxY_19 = boxY_19+ speed;
+//
+//     if (boxY_19 > 600) {
+//       boxY_19 = 0;
+//     }
+//     if (boxY_19 >= boxY_19Original-2 && boxY_19 <= boxY_19Original+2) {
+//       speed = 0;
+//     }
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   rect(97, boxY_20, 12, 42)
+//   boxY_20 = boxY_20+ speed2;
+//
+//     if (boxY_20 > 600) {
+//       boxY_20 = 0;
+//     }
+//     if (boxY_20 >= boxY_20Original-2 && boxY_20 <= boxY_20Original+2) {
+//       speed2 = 0;
+//     }
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   rect(98, boxY_21, 10, 38)
+//   boxY_21 = boxY_21+ speed3;
+//
+//     if (boxY_21 > 600) {
+//       boxY_21 = 0;
+//     }
+//     if (boxY_21 >= boxY_21Original-2 && boxY_21 <= boxY_21Original+2) {
+//       speed3 = 0;
+//     }
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   rect(493, boxY_22, 12, 42)
+//   boxY_22 = boxY_22+ speed;
+//
+//     if (boxY_22 > 600) {
+//       boxY_22 = 0;
+//     }
+//     if (boxY_22 >= boxY_22Original-2 && boxY_22 <= boxY_22Original+2) {
+//       speed = 0;
+//     }
+//
+//
+//
+//
+//   fill(color(133, 187, 101))
+//   rect(493, boxY_23, 10, 38)
+//   boxY_23 = boxY_23+ speed2;
+//
+//     if (boxY_23 > 600) {
+//       boxY_23 = 0;
+//     }
+//     if (boxY_23 >= boxY_23Original-2 && boxY_23 <= boxY_23Original+2) {
+//       speed2 = 0;
+//     }
+//
+//
+//
+// }
